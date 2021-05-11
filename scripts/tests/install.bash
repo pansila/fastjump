2363851366b989bf446e4a5a8a6dcea00e070543
Lin Zhou <xeontz@gmail.com>
2021-05-11 23:26:05 +0800

    fix: macos has no apt-get

--- scripts/tests/install.bash
+++ scripts/tests/install.bash
@@ -1,17 +1,29 @@
 #!/bin/sh
 
-sudo -h >/dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-    sudo apt-get install -y bash >/dev/null 2>&1
+if [ $TRAVIS_OS_NAME = linux ]; then
+    test_linux
 else
-    apt-get install -y bash >/dev/null 2>&1
+    test_macos
 fi
 
-if [ $? -ne 0 ]; then
-    echo "Failed to install bash" > 2
-    exit 1;
-fi
+test_linux() {
+    sudo -h >/dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        sudo apt-get install -y bash >/dev/null
+    else
+        apt-get install -y bash >/dev/null
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to install bash" > &2
+        exit 1
+    fi
+}
+
+test_macos() {
+
+}
 
 target="target/$TARGET/debug/install"
 
@@ -19,7 +31,7 @@ ret=$($target --install 2>&1)
 
 src=$(echo "$ret" | tail -n 1 | head -n 1 | awk '{print $(NF)}')
 if [ -z $src ]; then
-    echo "can not find the source file" > 2
+    echo "can not find the source file" > &2
     exit 1
 fi
 
